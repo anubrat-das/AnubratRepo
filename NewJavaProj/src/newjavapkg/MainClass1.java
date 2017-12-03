7e749e46c2c8c819d5b33ac854b1189f4acbc7cd
anubrat <anubrat@yahoo.com>
2017-12-03 09:28:49 +0530

    Save MainClass

---
+++ NewJavaProj/src/newjavapkg/MainClass1.java
@@ -0,0 +1,10 @@
+package newjavapkg;
+
+public class MainClass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Main class ssysout - 2");
+	}
+
+}
